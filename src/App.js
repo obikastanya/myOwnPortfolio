915f6fc1141b2c69ef568ec3c42d4293c4e1e05c
obikastanya <obikastanya@gmail.com>
2020-03-05 21:07:31 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    < div style={{ height: '300px', position: 'relative' }}>
+      <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover' }}>
+        <Header transparent title="Title" style={{ color: 'white' }}>
+          <Navigation>
+            <Link to="/skill">Skill</Link>
+            <Link to="/myProject">My Project</Link>
+            <Link to="/resume">Resume</Link>
+            <Link to="/contact">Contact Me</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <Link to="/skill">Skill</Link>
+            <Link to="/myProject">My Project</Link>
+            <Link to="resume">Resume</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
+        </Drawer>
+        <Content />
+      </Layout>
+    </div >);
 }
 
 export default App;
